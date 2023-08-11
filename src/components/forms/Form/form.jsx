f47194ddd91775d552004eceec48b4ece72ae5d2
Alana Maia <alanaalvesmaia@gmail.com>
2023-08-11 15:48:07 -0300

    Remoção de comentários.

--- src/components/forms/Form/form.jsx
+++ src/components/forms/Form/form.jsx
@@ -1,16 +1,3 @@
-// export const Form = ({ children, ...rest }) => {
-//   return (
-//     <form {...rest}>
-//       <div>
-//         <div>
-//           <h2>{title}</h2>
-//           {text !== "" ? <span>{text}</span> : null}
-//         </div>
-//         <div>{children}</div>
-//       </div>
-//     </form>
-//   );
-// };
 import styles from "./style.module.scss";
 
 export const Form = ({ children, title, text, handleSubmit, submit }) => {
